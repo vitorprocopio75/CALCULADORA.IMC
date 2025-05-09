ff4eb68dc1e1ed87aac30181c2c53db97bbeef30
vitorprocopio75 <vitor.nascimento.pontes@gmail.com>
2025-05-09 17:02:20 -0300

    calculadora imcp.ython

---
+++ calculadora_imc.py
@@ -0,0 +1,7 @@
+# fazer uma calculaldora para calcular o imc de uma pessoa 
+peso = float(input("Digite o seu peso"))
+#2 recebert a variavel altura 
+altura = float(input("Digite a sua altura"))
+imc = (peso/(altura** 2))
+print("Esse é o seu  Índice de Massa Corporal:",imc)
+#+
